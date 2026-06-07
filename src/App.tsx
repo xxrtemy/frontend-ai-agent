7abda094ecc51206f92503b088f13288e6fbf100
westalex777 <westalex777@yandex.ru>
2026-06-07 18:27:33 +0300

    task-05

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Routes from './routes.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Routes />
       <section id="center">
         <div className="hero">
 
